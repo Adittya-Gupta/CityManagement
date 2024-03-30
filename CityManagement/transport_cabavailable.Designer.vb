a304fb9d08bc99cad3aab809b2cd64ea441efebd
KannanRustagi <rustagi.kannan@gmail.com>
2024-03-30 19:40:37 +0530

    added cab driver info page

--- CityManagement/transport_cabavailable.Designer.vb
+++ CityManagement/transport_cabavailable.Designer.vb
@@ -29,132 +29,23 @@ Partial Class transport_cabavailable
         PictureBox1 = New PictureBox()
         PictureBox2 = New PictureBox()
         FlowLayoutPanel1 = New FlowLayoutPanel()
+        Panel1 = New Panel()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        Panel2 = New Panel()
+        Panel3 = New Panel()
+        DateTimePicker1 = New DateTimePicker()
+        Label1 = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        FlowLayoutPanel1.SuspendLayout()
+        Panel1.SuspendLayout()
+        Panel3.SuspendLayout()
         SuspendLayout()
-        ' 
-        ' Panel1
-        ' 
-        Panel1.BackColor = SystemColors.ButtonHighlight
-        Panel1.Controls.Add(Label7)
-        Panel1.Controls.Add(Label6)
-        Panel1.Controls.Add(Label5)
-        Panel1.Controls.Add(Label4)
-        Panel1.Controls.Add(Label3)
-        Panel1.Controls.Add(Panel2)
-        Panel1.Location = New Point(193, 54)
-        Panel1.Name = "Panel1"
-        Panel1.Size = New Size(922, 193)
-        Panel1.TabIndex = 0
-        ' 
-        ' Label7
-        ' 
-        Label7.AutoSize = True
-        Label7.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label7.Location = New Point(519, 136)
-        Label7.Name = "Label7"
-        Label7.Size = New Size(94, 28)
-        Label7.TabIndex = 5
-        Label7.Text = "RK Plaza"
-        ' 
-        ' Label6
-        ' 
-        Label6.AutoSize = True
-        Label6.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label6.Location = New Point(289, 136)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(128, 28)
-        Label6.TabIndex = 4
-        Label6.Text = "Rajiv Chowk"
-        ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label5.Location = New Point(519, 89)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(88, 23)
-        Label5.TabIndex = 3
-        Label5.Text = "11:00 AM"
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label4.Location = New Point(314, 89)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(82, 25)
-        Label4.TabIndex = 2
-        Label4.Text = "8:00 AM"
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label3.Location = New Point(58, 110)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(76, 28)
-        Label3.TabIndex = 1
-        Label3.Text = "150 Rs" & vbCrLf
-        ' 
-        ' Panel2
-        ' 
-        Panel2.BackColor = Color.LightBlue
-        Panel2.Location = New Point(0, 0)
-        Panel2.Name = "Panel2"
-        Panel2.Size = New Size(922, 37)
-        Panel2.TabIndex = 0
-        ' 
-        ' Panel3
-        ' 
-        Panel3.BackColor = Color.DarkGray
-        Panel3.Controls.Add(DateTimePicker1)
-        Panel3.Controls.Add(Label1)
-        Panel3.Location = New Point(190, 75)
-        Panel3.Name = "Panel3"
-        Panel3.Size = New Size(922, 45)
-        Panel3.TabIndex = 1
-        ' 
-        ' DateTimePicker1
-        ' 
-        DateTimePicker1.Location = New Point(122, 5)
-        DateTimePicker1.Name = "DateTimePicker1"
-        DateTimePicker1.Size = New Size(191, 27)
-        DateTimePicker1.TabIndex = 2
-        ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI Semibold", 9.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(15, 10)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(91, 20)
-        Label1.TabIndex = 0
-        Label1.Text = "DEPARTURE"
-        ' 
-        ' Button1
-        ' 
-        Button1.BackColor = Color.Firebrick
-        Button1.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold)
-        Button1.ForeColor = SystemColors.ControlLightLight
-        Button1.Location = New Point(466, 269)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(128, 46)
-        Button1.TabIndex = 2
-        Button1.Text = "Book"
-        Button1.UseVisualStyleBackColor = False
-        ' 
-        ' Button2
-        ' 
-        Button2.BackColor = Color.DarkOliveGreen
-        Button2.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold)
-        Button2.ForeColor = Color.White
-        Button2.Location = New Point(660, 269)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(172, 46)
-        Button2.TabIndex = 3
-        Button2.Text = "Look for Another"
-        Button2.UseVisualStyleBackColor = False
         ' 
         ' Label2
         ' 
@@ -164,7 +55,7 @@ Partial Class transport_cabavailable
         Label2.ForeColor = Color.Silver
         Label2.Location = New Point(80, 8)
         Label2.Name = "Label2"
-        Label2.Size = New Size(174, 22)
+        Label2.Size = New Size(206, 27)
         Label2.TabIndex = 11
         Label2.Text = "Transport Services"
         ' 
@@ -176,7 +67,7 @@ Partial Class transport_cabavailable
         Label8.ForeColor = Color.Silver
         Label8.Location = New Point(308, 10)
         Label8.Name = "Label8"
-        Label8.Size = New Size(43, 22)
+        Label8.Size = New Size(52, 27)
         Label8.TabIndex = 12
         Label8.Text = "Cab"
         ' 
@@ -186,9 +77,9 @@ Partial Class transport_cabavailable
         Label9.BackColor = Color.Transparent
         Label9.Font = New Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         Label9.ForeColor = Color.Silver
-        Label9.Location = New Point(447, 9)
+        Label9.Location = New Point(406, 8)
         Label9.Name = "Label9"
-        Label9.Size = New Size(136, 22)
+        Label9.Size = New Size(182, 29)
         Label9.TabIndex = 13
         Label9.Text = "Available Ride"
         ' 
@@ -217,10 +108,124 @@ Partial Class transport_cabavailable
         ' FlowLayoutPanel1
         ' 
         FlowLayoutPanel1.AutoScroll = True
+        FlowLayoutPanel1.Controls.Add(Panel1)
         FlowLayoutPanel1.Location = New Point(80, 46)
         FlowLayoutPanel1.Name = "FlowLayoutPanel1"
         FlowLayoutPanel1.Size = New Size(1107, 459)
         FlowLayoutPanel1.TabIndex = 16
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.ButtonHighlight
+        Panel1.Controls.Add(Panel3)
+        Panel1.Controls.Add(Label7)
+        Panel1.Controls.Add(Label6)
+        Panel1.Controls.Add(Label5)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(Panel2)
+        Panel1.Location = New Point(3, 3)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(922, 193)
+        Panel1.TabIndex = 3
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Exo 2", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(519, 136)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(92, 24)
+        Label7.TabIndex = 5
+        Label7.Text = "RK Plaza"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Exo 2", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(289, 136)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(125, 24)
+        Label6.TabIndex = 4
+        Label6.Text = "Rajiv Chowk"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Exo 2", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(519, 89)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(79, 20)
+        Label5.TabIndex = 3
+        Label5.Text = "11:00 AM"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Exo 2", 10.7999992F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(314, 89)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(80, 22)
+        Label4.TabIndex = 2
+        Label4.Text = "8:00 AM"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Exo 2", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(58, 109)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(71, 24)
+        Label3.TabIndex = 1
+        Label3.Text = "150 Rs" & vbCrLf
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Tomato
+        Button1.Font = New Font("Exo 2", 9.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = SystemColors.ControlLightLight
+        Button1.Location = New Point(777, 136)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(128, 45)
+        Button1.TabIndex = 2
+        Button1.Text = "Book"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.LightBlue
+        Panel2.Location = New Point(0, 0)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(922, 35)
+        Panel2.TabIndex = 0
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.DarkGray
+        Panel3.Controls.Add(DateTimePicker1)
+        Panel3.Controls.Add(Label1)
+        Panel3.Location = New Point(0, 41)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(922, 45)
+        Panel3.TabIndex = 6
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(122, 5)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(191, 27)
+        DateTimePicker1.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Exo 2", 9.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(15, 11)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(94, 18)
+        Label1.TabIndex = 0
+        Label1.Text = "DEPARTURE"
         ' 
         ' transport_cabavailable
         ' 
@@ -233,14 +238,15 @@ Partial Class transport_cabavailable
         Controls.Add(Label9)
         Controls.Add(Label8)
         Controls.Add(Label2)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
-        Controls.Add(Panel3)
-        Controls.Add(Panel1)
-        Name = "Form1"
+        Name = "transport_cabavailable"
         Text = "Form1"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        FlowLayoutPanel1.ResumeLayout(False)
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -250,4 +256,15 @@ Partial Class transport_cabavailable
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
 End Class

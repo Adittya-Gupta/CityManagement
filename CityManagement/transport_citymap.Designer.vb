b0d64d1b43dc4c1fba0339a5d5c9c1d6fb54cab8
KannanRustagi <rustagi.kannan@gmail.com>
2024-04-02 00:58:29 +0530

    added city map

---
+++ CityManagement/transport_citymap.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class transport_citymap
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(transport_citymap))
+        Panel1 = New Panel()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        Panel1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Black
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(148, 1)
+        Panel1.Margin = New Padding(3, 4, 3, 4)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1056, 84)
+        Panel1.TabIndex = 7
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Yu Gothic", 21.75F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(175, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(668, 48)
+        Label1.TabIndex = 0
+        Label1.Text = "Welcome to Transportation Module!"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(223, 92)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(922, 565)
+        PictureBox1.TabIndex = 8
+        PictureBox1.TabStop = False
+        ' 
+        ' transport_citymap
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1401, 669)
+        Controls.Add(PictureBox1)
+        Controls.Add(Panel1)
+        Name = "transport_citymap"
+        Text = "transport_citymap"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
--- CityManagement/transport_landingPage.vb
+++ CityManagement/transport_landingPage.vb
@@ -3,10 +3,10 @@
         ' Hide the current form
 
         ' Create an instance of Form2
-        Dim form2 As New transport_busSearch()
+        Dim form2 As New transport_busSearch
 
         ' Show Form2
-        form2.Show()
+        form2.Show
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
@@ -17,5 +17,15 @@
 
         ' Show Form2
         form2.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim form2 As New transport_citymap()
+
+        ' Show Form2
+        Me.Hide()
+
+        form2.ShowDialog()
+        Me.Show()
     End Sub
 End Class

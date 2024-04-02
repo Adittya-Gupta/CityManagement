008710460169016e5a72d7891e0671d9e65d4942
KannanRustagi <rustagi.kannan@gmail.com>
2024-04-02 16:59:30 +0530

    changed starter form

--- CityManagement/My Project/Application.Designer.vb
+++ CityManagement/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CityManagement.professor_dashboard
+            Me.MainForm = Global.CityManagement.WelcomeScreen
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _

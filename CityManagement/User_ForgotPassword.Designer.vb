73c0cebc441a6039aa3c6ebc1c0fe869022b9795
Darshan Patni <p.darshan@iitg.ac.in>
2024-04-05 15:34:09 +0530

    Resend OTP functionality added

--- CityManagement/User_ForgotPassword.Designer.vb
+++ CityManagement/User_ForgotPassword.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ForgotPassword
     Inherits System.Windows.Forms.Form
 
@@ -24,6 +24,7 @@ Partial Class ForgotPassword
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ForgotPassword))
         Panel1 = New Panel()
+        ResendLabel = New Label()
         Label9 = New Label()
         Label8 = New Label()
         Label7 = New Label()
@@ -36,7 +37,7 @@ Partial Class ForgotPassword
         Label4 = New Label()
         Label1 = New Label()
         Label2 = New Label()
-        TimeLeftLabel = New Label()
+        Label6 = New Label()
         Email_tb = New TextBox()
         TextBox1 = New TextBox()
         Button2 = New Button()
@@ -49,6 +50,7 @@ Partial Class ForgotPassword
         ' 
         ' Panel1
         ' 
+        Panel1.Controls.Add(ResendLabel)
         Panel1.Controls.Add(Label9)
         Panel1.Controls.Add(Label8)
         Panel1.Controls.Add(Label7)
@@ -59,7 +61,7 @@ Partial Class ForgotPassword
         Panel1.Controls.Add(Label4)
         Panel1.Controls.Add(Label1)
         Panel1.Controls.Add(Label2)
-        Panel1.Controls.Add(TimeLeftLabel)
+        Panel1.Controls.Add(Label6)
         Panel1.Controls.Add(Email_tb)
         Panel1.Controls.Add(TextBox1)
         Panel1.Controls.Add(Button2)
@@ -71,15 +73,27 @@ Partial Class ForgotPassword
         Panel1.Size = New Size(917, 1100)
         Panel1.TabIndex = 7
         ' 
+        ' ResendLabel
+        ' 
+        ResendLabel.Anchor = AnchorStyles.None
+        ResendLabel.AutoSize = True
+        ResendLabel.Location = New Point(542, 469)
+        ResendLabel.Name = "ResendLabel"
+        ResendLabel.Size = New Size(196, 20)
+        ResendLabel.TabIndex = 54
+        ResendLabel.Text = "Having Issues? Resend Code"
+        ' 
         ' Label9
         ' 
-        Label9.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label9.Anchor = AnchorStyles.None
+        Label9.Font = New Font("Microsoft Sans Serif", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label9.Location = New Point(298, 884)
         Label9.Name = "Label9"
         Label9.Size = New Size(350, 31)
         Label9.TabIndex = 53
         Label9.Text = "We just emailed you an OTP to reset the password."
         Label9.TextAlign = ContentAlignment.MiddleCenter
+        Label9.Visible = False
         ' 
         ' Label8
         ' 
@@ -93,6 +107,7 @@ Partial Class ForgotPassword
         Label8.TabIndex = 52
         Label8.Text = "Check in your mail ! "
         Label8.TextAlign = ContentAlignment.MiddleCenter
+        Label8.Visible = False
         ' 
         ' Label7
         ' 
@@ -145,6 +160,7 @@ Partial Class ForgotPassword
         ' 
         ' PictureBox3
         ' 
+        PictureBox3.Anchor = AnchorStyles.None
         PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), Image)
         PictureBox3.BackgroundImageLayout = ImageLayout.Stretch
         PictureBox3.Location = New Point(276, 91)
@@ -155,6 +171,7 @@ Partial Class ForgotPassword
         ' 
         ' PictureBox1
         ' 
+        PictureBox1.Anchor = AnchorStyles.None
         PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
         PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
         PictureBox1.Location = New Point(276, 640)
@@ -162,19 +179,23 @@ Partial Class ForgotPassword
         PictureBox1.Size = New Size(389, 194)
         PictureBox1.TabIndex = 46
         PictureBox1.TabStop = False
+        PictureBox1.Visible = False
         ' 
         ' Label5
         ' 
-        Label5.Font = New Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label5.Anchor = AnchorStyles.None
+        Label5.Font = New Font("Microsoft Sans Serif", 10.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label5.Location = New Point(319, 557)
         Label5.Name = "Label5"
         Label5.Size = New Size(256, 23)
         Label5.TabIndex = 44
         Label5.Text = "Time left before code expires :"
+        Label5.Visible = False
         ' 
         ' Label4
         ' 
-        Label4.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.Anchor = AnchorStyles.None
+        Label4.Font = New Font("Microsoft Sans Serif", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label4.Location = New Point(298, 358)
         Label4.Name = "Label4"
         Label4.Size = New Size(350, 48)
@@ -203,19 +224,22 @@ Partial Class ForgotPassword
         Label2.Size = New Size(0, 22)
         Label2.TabIndex = 13
         ' 
-        ' TimeLeftLabel
+        ' Label6
         ' 
-        TimeLeftLabel.AutoSize = True
-        TimeLeftLabel.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        TimeLeftLabel.Location = New Point(319, 590)
-        TimeLeftLabel.Name = "TimeLeftLabel"
-        TimeLeftLabel.Size = New Size(121, 25)
-        TimeLeftLabel.TabIndex = 12
-        TimeLeftLabel.Text = "Time Left : "
+        Label6.Anchor = AnchorStyles.None
+        Label6.AutoSize = True
+        Label6.Font = New Font("Microsoft Sans Serif", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(319, 590)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(121, 25)
+        Label6.TabIndex = 12
+        Label6.Text = "Time Left : "
+        Label6.Visible = False
         ' 
         ' Email_tb
         ' 
-        Email_tb.Font = New Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Email_tb.Anchor = AnchorStyles.None
+        Email_tb.Font = New Font("Microsoft Sans Serif", 10.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Email_tb.Location = New Point(315, 425)
         Email_tb.Margin = New Padding(3, 4, 3, 4)
         Email_tb.Name = "Email_tb"
@@ -225,20 +249,23 @@ Partial Class ForgotPassword
         ' 
         ' TextBox1
         ' 
-        TextBox1.Font = New Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Anchor = AnchorStyles.None
+        TextBox1.Font = New Font("Microsoft Sans Serif", 10.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         TextBox1.Location = New Point(315, 936)
         TextBox1.Margin = New Padding(3, 4, 3, 4)
         TextBox1.Name = "TextBox1"
         TextBox1.PlaceholderText = "OTP"
         TextBox1.Size = New Size(309, 26)
         TextBox1.TabIndex = 9
+        TextBox1.Visible = False
         ' 
         ' Button2
         ' 
+        Button2.Anchor = AnchorStyles.None
         Button2.BackColor = Color.Black
         Button2.FlatAppearance.BorderSize = 0
         Button2.FlatStyle = FlatStyle.Flat
-        Button2.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Font = New Font("Microsoft Sans Serif", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button2.ForeColor = Color.White
         Button2.Location = New Point(387, 970)
         Button2.Margin = New Padding(3, 4, 3, 4)
@@ -247,13 +274,15 @@ Partial Class ForgotPassword
         Button2.TabIndex = 8
         Button2.Text = "VALIDATE"
         Button2.UseVisualStyleBackColor = False
+        Button2.Visible = False
         ' 
         ' Button1
         ' 
+        Button1.Anchor = AnchorStyles.None
         Button1.BackColor = Color.Black
         Button1.FlatAppearance.BorderSize = 0
         Button1.FlatStyle = FlatStyle.Flat
-        Button1.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Font = New Font("Microsoft Sans Serif", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button1.ForeColor = Color.White
         Button1.Location = New Point(384, 459)
         Button1.Margin = New Padding(3, 4, 3, 4)
@@ -284,7 +313,7 @@ Partial Class ForgotPassword
     End Sub
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents TimeLeftLabel As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Email_tb As System.Windows.Forms.TextBox
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button2 As System.Windows.Forms.Button
@@ -300,4 +329,5 @@ Partial Class ForgotPassword
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
+    Friend WithEvents ResendLabel As Label
 End Class
--- CityManagement/User_ForgotPassword.vb
+++ CityManagement/User_ForgotPassword.vb
@@ -4,17 +4,20 @@ Imports System.Text
 Imports System.Net
 Imports System.Net.Mail
 Imports System.Windows.Forms
+
 Public Class ForgotPassword
     Dim connString As String = "server=172.16.114.244;userid=admin;Password=nimda;database=smart_city_management;sslmode=none"
     Dim conn As New MySqlConnection(connString)
-    'Dim EmailAddress As String = Environment.GetEnvironmentVariable("Email")
     Dim randomCode As String
     Dim elapsedTime As Integer = 0 ' Track elapsed time in seconds
+    Dim incorrectAttempts As Integer = 0 ' Track incorrect attempts
     Dim WithEvents timer As New Timer()
 
     Private Sub ForgotPassword_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Label2.Hide()
+        ResendLabel.Visible = False ' Hide the resend label initially
     End Sub
+
     Public Function IsEmailPresent(ByVal EmailAddress As String) As Boolean
         Dim isPresent As Boolean = False
 
@@ -56,17 +59,34 @@ Public Class ForgotPassword
     ' Timer tick event handler to track elapsed time
     Private Sub Timer_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles timer.Tick
         elapsedTime += 1 ' Increment elapsed time by 1 second
-        Dim timeLeft As Integer = 60 - elapsedTime
-        TimeLeftLabel.Text = "Time Left: " & timeLeft.ToString() & " seconds"
-        If elapsedTime >= 60 Then ' Check if 60 seconds have elapsed
+
+        ' Calculate remaining time
+        Dim timeLeft As Integer = 180 - elapsedTime
+
+        ' Update the TimeLeftLabel with the remaining time
+        Label6.Text = "Time Left: " & timeLeft.ToString() & " seconds"
+
+        ' Check if 30 seconds have elapsed without entering the code
+        If elapsedTime >= 30 AndAlso TextBox1.Text = "" Then
+            ShowResendLabel()
+        End If
+
+        ' Check if the user has entered an incorrect code three times
+        If incorrectAttempts >= 3 Then
+            ShowResendLabel()
+        End If
+
+        ' Check if 180 seconds have elapsed
+        If elapsedTime >= 180 Then
             timer.Stop() ' Stop the timer
             MessageBox.Show("Verification code has expired. Please request a new one by clicking Send Code again.")
-
+            Button1.Visible = True ' Make the Send Code button visible again
         End If
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-
+        Button1.Visible = False ' Hide the Send Code button
+        Button1.FlatStyle = FlatStyle.Flat
         Label2.Visible = True
         Label2.Text = "Sending code....."
         Label2.ForeColor = Color.Black
@@ -76,16 +96,15 @@ Public Class ForgotPassword
         If String.IsNullOrEmpty(EmailAddress) Then
             MessageBox.Show("Please enter your Email to send code")
             Label2.Hide()
-
+            Button1.Visible = True ' Show the Send Code button again
             Return
         End If
 
         If Not IsEmailPresent(EmailAddress) Then
             Label2.Hide()
-
+            Button1.Visible = True ' Show the Send Code button again
             Return
         End If
-
 
         Dim rand As New Random()
         randomCode = (rand.Next(999999)).ToString()
@@ -108,15 +127,21 @@ Public Class ForgotPassword
                 SmtpServer.Send(mail)
                 MessageBox.Show("Please check your Email for the verification code and enter it below!")
                 timer.Start() ' Start the timer
+                Label5.Visible = True
+                Label6.Visible = True
+                Label8.Visible = True
+                Label9.Visible = True
+                TextBox1.Visible = True
+                PictureBox1.Visible = True
+                Button2.Visible = True
             Catch ex As Exception
-                'Label2.Visible = True
-                Label2.Text = "Unexpected Error occured!! Please click on send code again!"
+                ' Label2.Visible = True
+                Label2.Text = "Unexpected Error occurred!! Please click on send code again!"
                 Label2.ForeColor = Color.Red
-                MessageBox.Show("Error occured sending code :", ex.Message)
-
+                MessageBox.Show("Error occurred sending code: " & ex.Message)
+                Button1.Visible = True ' Show the Send Code button again
             End Try
 
-            'Label2.Visible = True
             Label2.Text = "Code sent!" & Environment.NewLine & "Please check your Email for the code and enter it below!"
             Label2.ForeColor = Color.Green
 
@@ -131,21 +156,18 @@ Public Class ForgotPassword
             Return
         End If
 
-        If elapsedTime < 60 Then ' Check if the elapsed time is less than 60 seconds
+        If elapsedTime < 180 Then ' Check if the elapsed time is less than 180 seconds
             If TextBox1.Text.Equals(randomCode) Then
                 timer.Stop()
                 switchPanel(User_ChangePassword)
-                'Dim resetPassword As New User_ChangePassword()
-                'resetPassword.Show()
-                'Me.Hide()
-
             ElseIf elapsedTime = 0 Then
                 MessageBox.Show("Please click on send code to get a code")
                 Return
             Else
-                Label2.Text = "Incorrect code."
+                incorrectAttempts += 1
+                Label2.Text = "Incorrect code, make sure that you are entering the latest code you have received."
                 Label2.ForeColor = Color.Red
-                MessageBox.Show("Incorrect code.")
+                MessageBox.Show("Incorrect code, make sure that you are entering the latest code you have received.")
             End If
         Else
             Label2.Text = "Verification code has expired. Please request a new one by clicking on Send Code again."
@@ -154,8 +176,23 @@ Public Class ForgotPassword
         End If
     End Sub
 
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+    ' Method to show the resend label
+    Private Sub ShowResendLabel()
+        ResendLabel.Visible = True
+        Label2.Text = ""
 
+    End Sub
+
+    ' Method to resend the code when the resend label is clicked
+    Private Sub ResendLabel_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ResendLabel.Click
+        ResendLabel.Visible = False ' Hide the resend label
+        ' Label2.Visible = True
+        ' Reset necessary variables and resend the code
+        incorrectAttempts = 0
+        elapsedTime = 0
+        Button1.Visible = True
+        Button1.PerformClick() ' Simulate a click on the "Send Code" button to resend the code
+        ' Label2.Visible = True
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
@@ -167,5 +204,7 @@ Public Class ForgotPassword
         Me.Close()
     End Sub
 
-End Class
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
 
+    End Sub
+End Class

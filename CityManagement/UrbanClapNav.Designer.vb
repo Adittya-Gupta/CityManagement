7ff5c480da6e654f5b7cb7a09bffd98b8768003f
Rashaad10 <baigrashaad@gmail.com>
2024-04-13 12:55:45 +0530

    UrbanClap

--- CityManagement/UrbanClapNav.Designer.vb
+++ CityManagement/UrbanClapNav.Designer.vb
@@ -36,7 +36,7 @@ Partial Class UrbanClapNav
         Label1.BackColor = Color.Black
         Label1.Location = New Point(0, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(1254, 73)
+        Label1.Size = New Size(1108, 73)
         Label1.TabIndex = 0
         Label1.Text = "Label1"
         ' 
@@ -45,9 +45,9 @@ Partial Class UrbanClapNav
         Button1.BackColor = Color.Black
         Button1.FlatAppearance.BorderSize = 0
         Button1.FlatStyle = FlatStyle.Flat
-        Button1.Font = New Font("Exo 2", 24.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Font = New Font("Exo 2", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button1.ForeColor = Color.White
-        Button1.Location = New Point(113, 0)
+        Button1.Location = New Point(24, 0)
         Button1.Name = "Button1"
         Button1.Size = New Size(333, 73)
         Button1.TabIndex = 1
@@ -59,11 +59,11 @@ Partial Class UrbanClapNav
         Button2.BackColor = Color.Black
         Button2.FlatAppearance.BorderSize = 0
         Button2.FlatStyle = FlatStyle.Flat
-        Button2.Font = New Font("Exo 2", 24.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Font = New Font("Exo 2", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button2.ForeColor = Color.White
-        Button2.Location = New Point(494, 0)
+        Button2.Location = New Point(439, 0)
         Button2.Name = "Button2"
-        Button2.Size = New Size(327, 73)
+        Button2.Size = New Size(236, 73)
         Button2.TabIndex = 2
         Button2.Text = "Chats"
         Button2.UseVisualStyleBackColor = False
@@ -72,7 +72,7 @@ Partial Class UrbanClapNav
         ' 
         CurvedLabel1.BackColor = Color.White
         CurvedLabel1.CornerRadius = 5
-        CurvedLabel1.Location = New Point(135, -5)
+        CurvedLabel1.Location = New Point(45, -5)
         CurvedLabel1.Name = "CurvedLabel1"
         CurvedLabel1.Size = New Size(287, 11)
         CurvedLabel1.TabIndex = 3
@@ -81,16 +81,16 @@ Partial Class UrbanClapNav
         ' 
         CurvedLabel2.BackColor = Color.White
         CurvedLabel2.CornerRadius = 5
-        CurvedLabel2.Location = New Point(598, -5)
+        CurvedLabel2.Location = New Point(493, -5)
         CurvedLabel2.Name = "CurvedLabel2"
         CurvedLabel2.Size = New Size(119, 11)
         CurvedLabel2.TabIndex = 5
         ' 
         ' Panel1
         ' 
-        Panel1.Location = New Point(0, 73)
+        Panel1.Location = New Point(0, 76)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(1254, 953)
+        Panel1.Size = New Size(1107, 685)
         Panel1.TabIndex = 6
         ' 
         ' Button3
@@ -99,21 +99,21 @@ Partial Class UrbanClapNav
         Button3.Cursor = Cursors.Hand
         Button3.FlatAppearance.BorderSize = 0
         Button3.FlatStyle = FlatStyle.Flat
-        Button3.Font = New Font("Exo 2", 24.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button3.Font = New Font("Exo 2", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button3.ForeColor = Color.White
-        Button3.Location = New Point(859, 0)
+        Button3.Location = New Point(721, 0)
         Button3.Name = "Button3"
-        Button3.Size = New Size(294, 73)
+        Button3.Size = New Size(363, 73)
         Button3.TabIndex = 7
         Button3.Text = "Work Section"
         Button3.UseVisualStyleBackColor = False
         ' 
         ' UrbanClapNav
         ' 
-        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.FromArgb(CByte(248), CByte(249), CByte(250))
-        ClientSize = New Size(1254, 1026)
+        ClientSize = New Size(1107, 760)
         Controls.Add(Button3)
         Controls.Add(Panel1)
         Controls.Add(CurvedLabel2)

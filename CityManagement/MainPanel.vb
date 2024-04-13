acd1d865ab8d32a3c56fd932ebe014e2fa4b7753
ADITTYA GUPTA <g.adittya@iitg.ac.in>
2024-04-13 17:07:45 +0530

    changes while merging

--- CityManagement/MainPanel.vb
+++ CityManagement/MainPanel.vb
@@ -10,7 +10,7 @@
         End If
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
+        ShowFormInPanel(specialisation)
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
@@ -35,7 +35,7 @@
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-
+        ShowFormInPanel(Globals.UrbanClapNavForm)
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
@@ -76,5 +76,36 @@
         form.TopLevel = False
         mypanel.Panel1.Controls.Add(form)
         form.Show()
+    End Sub
+    Public Sub ShowFormInPanel(formToShow As Form)
+        ' Clear Panel1
+        Me.Panel1.Controls.Clear()
+
+        ' Set the properties of the form to be displayed inside Panel1
+        formToShow.TopLevel = False
+        formToShow.FormBorderStyle = FormBorderStyle.None
+        formToShow.Dock = DockStyle.Fill
+
+        ' Add the form to Panel1
+        Me.Panel1.Controls.Add(formToShow)
+
+        ' Show the form
+        formToShow.Show()
+
+    End Sub
+    Public Sub ShowChildFormInPanel(formToShow As Form)
+        ' Check if the panel already contains any controls
+        Me.Panel1.Controls.Clear()
+
+        ' Set the properties of the form to be displayed inside Panel1
+        formToShow.TopLevel = False
+        formToShow.FormBorderStyle = FormBorderStyle.None
+        formToShow.Dock = DockStyle.Fill
+
+        ' Add the form to Panel1
+        Me.Panel1.Controls.Add(formToShow)
+
+        ' Show the form
+        formToShow.Show()
     End Sub
 End Class

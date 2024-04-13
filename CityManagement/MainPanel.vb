c7199060dab215c77cd3c54291fdfccb82f3481f
ShallyKandoi <95773712+ShallyKandoi@users.noreply.github.com>
2024-04-13 22:05:53 +0530

    mainpanel changes reverted

--- CityManagement/MainPanel.vb
+++ CityManagement/MainPanel.vb
@@ -1,5 +1,9 @@
 ﻿Public Class MainPanel
 
+    'Make this form full screen
+    Private Sub MainPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
 
     End Sub
@@ -17,31 +21,18 @@
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        ShowFormInPanel(Globals.UrbanClapNavForm)
+
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        ShowFormInPanel(New EmploymentPortal())
+
+    End Sub
+
+    Private Sub PictureBox10_Click(sender As Object, e As EventArgs) Handles PictureBox10.Click
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
-
-    Public Sub ShowFormInPanel(formToShow As Form)
-        ' Clear Panel1
-        Me.Panel1.Controls.Clear()
-
-        ' Set the properties of the form to be displayed inside Panel1
-        formToShow.TopLevel = False
-        formToShow.FormBorderStyle = FormBorderStyle.None
-        formToShow.Dock = DockStyle.Fill
-
-        ' Add the form to Panel1
-        Me.Panel1.Controls.Add(formToShow)
-
-        ' Show the form
-        formToShow.Show()
 
     End Sub
 End Class

5ee63881f4fb116db8469a21b771764557c51e72
Ravi Lahare <ravilahare1912@gmail.com>
2024-04-13 13:21:00 +0530

    updated expiry label

--- Module 5/Module 5/Banking_Money_Management_Credit_Card.Designer.vb
+++ Module 5/Module 5/Banking_Money_Management_Credit_Card.Designer.vb
@@ -54,7 +54,7 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Margin = New System.Windows.Forms.Padding(0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(492, 513)
+        Me.Panel1.Size = New System.Drawing.Size(554, 641)
         Me.Panel1.TabIndex = 76
         '
         'Label6
@@ -62,9 +62,9 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.White
-        Me.Label6.Location = New System.Drawing.Point(5, 306)
+        Me.Label6.Location = New System.Drawing.Point(6, 382)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(380, 58)
+        Me.Label6.Size = New System.Drawing.Size(454, 69)
         Me.Label6.TabIndex = 3
         Me.Label6.Text = "With No Charge"
         '
@@ -73,9 +73,9 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.Color.White
-        Me.Label5.Location = New System.Drawing.Point(5, 246)
+        Me.Label5.Location = New System.Drawing.Point(6, 308)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(452, 58)
+        Me.Label5.Size = New System.Drawing.Size(542, 69)
         Me.Label5.TabIndex = 2
         Me.Label5.Text = "World In Real Time"
         '
@@ -84,9 +84,9 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(5, 186)
+        Me.Label4.Location = New System.Drawing.Point(6, 232)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(178, 58)
+        Me.Label4.Size = New System.Drawing.Size(213, 69)
         Me.Label4.TabIndex = 1
         Me.Label4.Text = "Across"
         '
@@ -95,9 +95,9 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(5, 133)
+        Me.Label3.Location = New System.Drawing.Point(6, 166)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(377, 58)
+        Me.Label3.Size = New System.Drawing.Size(448, 69)
         Me.Label3.TabIndex = 0
         Me.Label3.Text = "Transfer Money"
         '
@@ -115,20 +115,20 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Panel3.Controls.Add(Me.Label7)
         Me.Panel3.Controls.Add(Me.Label_Card)
         Me.Panel3.Controls.Add(Me.Label1)
-        Me.Panel3.Location = New System.Drawing.Point(492, 0)
+        Me.Panel3.Location = New System.Drawing.Point(554, 0)
         Me.Panel3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(492, 513)
+        Me.Panel3.Size = New System.Drawing.Size(554, 641)
         Me.Panel3.TabIndex = 77
         '
         'RichTextBox_Remark
         '
         Me.RichTextBox_Remark.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RichTextBox_Remark.Location = New System.Drawing.Point(33, 373)
+        Me.RichTextBox_Remark.Location = New System.Drawing.Point(37, 466)
         Me.RichTextBox_Remark.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.RichTextBox_Remark.Multiline = False
         Me.RichTextBox_Remark.Name = "RichTextBox_Remark"
-        Me.RichTextBox_Remark.Size = New System.Drawing.Size(432, 48)
+        Me.RichTextBox_Remark.Size = New System.Drawing.Size(486, 59)
         Me.RichTextBox_Remark.TabIndex = 24
         Me.RichTextBox_Remark.Text = ""
         '
@@ -136,42 +136,42 @@ Partial Class Banking_Money_Management_Credit_Card
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(28, 338)
+        Me.Label9.Location = New System.Drawing.Point(32, 422)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(97, 29)
+        Me.Label9.Size = New System.Drawing.Size(112, 32)
         Me.Label9.TabIndex = 23
         Me.Label9.Text = "Remark"
         '
         'RichTextBox_Cvv
         '
         Me.RichTextBox_Cvv.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RichTextBox_Cvv.Location = New System.Drawing.Point(35, 281)
+        Me.RichTextBox_Cvv.Location = New System.Drawing.Point(39, 351)
         Me.RichTextBox_Cvv.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.RichTextBox_Cvv.Multiline = False
         Me.RichTextBox_Cvv.Name = "RichTextBox_Cvv"
-        Me.RichTextBox_Cvv.Size = New System.Drawing.Size(432, 48)
+        Me.RichTextBox_Cvv.Size = New System.Drawing.Size(486, 59)
         Me.RichTextBox_Cvv.TabIndex = 22
         Me.RichTextBox_Cvv.Text = ""
         '
         'RichTextBox_Expiry
         '
         Me.RichTextBox_Expiry.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RichTextBox_Expiry.Location = New System.Drawing.Point(35, 186)
+        Me.RichTextBox_Expiry.Location = New System.Drawing.Point(39, 232)
         Me.RichTextBox_Expiry.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.RichTextBox_Expiry.Multiline = False
         Me.RichTextBox_Expiry.Name = "RichTextBox_Expiry"
-        Me.RichTextBox_Expiry.Size = New System.Drawing.Size(432, 48)
+        Me.RichTextBox_Expiry.Size = New System.Drawing.Size(486, 59)
         Me.RichTextBox_Expiry.TabIndex = 21
         Me.RichTextBox_Expiry.Text = ""
         '
         'RichTextBox_Card
         '
         Me.RichTextBox_Card.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RichTextBox_Card.Location = New System.Drawing.Point(35, 90)
+        Me.RichTextBox_Card.Location = New System.Drawing.Point(39, 112)
         Me.RichTextBox_Card.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.RichTextBox_Card.Multiline = False
         Me.RichTextBox_Card.Name = "RichTextBox_Card"
-        Me.RichTextBox_Card.Size = New System.Drawing.Size(432, 48)
+        Me.RichTextBox_Card.Size = New System.Drawing.Size(486, 59)
         Me.RichTextBox_Card.TabIndex = 20
         Me.RichTextBox_Card.Text = ""
         '
@@ -180,10 +180,10 @@ Partial Class Banking_Money_Management_Credit_Card
         Me.Button1.BackColor = System.Drawing.Color.Navy
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.White
-        Me.Button1.Location = New System.Drawing.Point(35, 434)
+        Me.Button1.Location = New System.Drawing.Point(39, 542)
         Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(431, 46)
+        Me.Button1.Size = New System.Drawing.Size(485, 58)
         Me.Button1.TabIndex = 19
         Me.Button1.Text = "Verify Password"
         Me.Button1.UseVisualStyleBackColor = False
@@ -192,9 +192,9 @@ Partial Class Banking_Money_Management_Credit_Card
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(29, 246)
+        Me.Label8.Location = New System.Drawing.Point(33, 308)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(60, 29)
+        Me.Label8.Size = New System.Drawing.Size(72, 32)
         Me.Label8.TabIndex = 18
         Me.Label8.Text = "CVV"
         '
@@ -202,19 +202,19 @@ Partial Class Banking_Money_Management_Credit_Card
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(29, 150)
+        Me.Label7.Location = New System.Drawing.Point(33, 188)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(135, 29)
+        Me.Label7.Size = New System.Drawing.Size(337, 32)
         Me.Label7.TabIndex = 17
-        Me.Label7.Text = "Expiry Date"
+        Me.Label7.Text = "Expiry Date (yyyy-mm-dd)"
         '
         'Label_Card
         '
         Me.Label_Card.AutoSize = True
         Me.Label_Card.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label_Card.Location = New System.Drawing.Point(29, 57)
+        Me.Label_Card.Location = New System.Drawing.Point(33, 71)
         Me.Label_Card.Name = "Label_Card"
-        Me.Label_Card.Size = New System.Drawing.Size(174, 29)
+        Me.Label_Card.Size = New System.Drawing.Size(201, 32)
         Me.Label_Card.TabIndex = 16
         Me.Label_Card.Text = "Credit Card No"
         '
@@ -222,17 +222,17 @@ Partial Class Banking_Money_Management_Credit_Card
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(25, 13)
+        Me.Label1.Location = New System.Drawing.Point(28, 16)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(202, 39)
+        Me.Label1.Size = New System.Drawing.Size(234, 46)
         Me.Label1.TabIndex = 15
         Me.Label1.Text = "Credit Card"
         '
         'Banking_Money_Management_Credit_Card
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(984, 513)
+        Me.ClientSize = New System.Drawing.Size(1107, 641)
         Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
